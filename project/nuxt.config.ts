62fba65cbaa41ad84e607e48891f9f4e422d7df5
M.Spinks <mikespinks0401@gmail.com>
2023-03-20 21:41:53 -0500

    update

--- project/nuxt.config.ts
+++ project/nuxt.config.ts
@@ -1,13 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     srcDir: "src",
+    ssr: false,
     css: ["vuetify/lib/styles/main.css", "@mdi/font/css/materialdesignicons.min.css"],
     modules:["@pinia/nuxt"],
     build:{
-        transpile:["vuetify"]
+        transpile:["vuetify"],
     },
     vite:{
-        base:"/project-restApi-countries/"
+        appType:"spa",
+        base:'/project-restApi-countries/',
+        build:{
+            
+        },
+        vue:{
+            
+        } 
     },
 
 })
