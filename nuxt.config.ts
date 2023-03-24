a3893543d2be2a5418c7f072134af02fe7aa99c4
Michael Spinks <mikespinks0401@gmail.com>
2023-03-24 10:04:31 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     css: ["vuetify/lib/styles/main.css", "@mdi/font/css/materialdesignicons.min.css"],
     modules:["@pinia/nuxt"],
     app:{
-        cdnURL: "https://mikespinks0401.github.io",
+        cdnURL: "https://mikespinks0401.github.io/project-restApi-countries",
         baseURL: "/project-restApi-countries/"
     },
     build:{
