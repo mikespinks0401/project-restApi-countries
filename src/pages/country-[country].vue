2dae5f39d7a01db8752942242088c2fe1ff548ed
M.Spinks <mikespinks0401@gmail.com>
2023-05-19 17:53:28 -0500

    added chips + createLink function

--- src/pages/country-[country].vue
+++ src/pages/country-[country].vue
@@ -6,17 +6,39 @@ const error = ref<string | null>(null)
 const useCountry = ref<countryType | null>(null)
 const route = useRoute()
 const countryName = route.params.country
-
+const useLanguage = ref('')
+const useCurrency = ref('')
+const useNative = ref('')
+const useBorders = ref([''])
 onMounted(() => {
-    loadCountry()
+    let link = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+    if (countryName.length < 4) {
+        link = `https://restcountries.com/v3.1/alpha/${countryName}`
+    }
+    loadCountry(link)
 })
 
-const loadCountry = async () => {
+
+
+const loadCountry = async (link: string) => {
     loading.value = true
     try {
-        const [country] = await $fetch<countryType[]>(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+        let [country] = await $fetch<countryType[]>(`${link}`)
         if (country) {
             useCountry.value = country
+
+            console.log(useCountry.value)
+            useBorders.value = useCountry.value.borders
+            console.log(useBorders.value)
+
+            //set native lanaguage
+            useNative.value = Object.values(useCountry.value.name.nativeName)[0].official
+            //set currency
+            let { currencies } = useCountry.value
+            useCurrency.value = Object.values(currencies)[0].name
+            //set language
+            let { languages } = useCountry.value
+            useLanguage.value = Object.values(languages)[0] as string
         }
     } catch (e) {
         error.value = "Error Loading The Country.Please Try Again"
@@ -37,28 +59,14 @@ const useCapital = computed(() => {
 })
 
 
-const useTopLevelDomains = computed(() => {
-    if (useCountry.value === null) {
-        return
-    }
-    let domains = ""
-    for (let ind in useCountry?.value.topLevelDomain) {
-        domains += useCountry.value.topLevelDomain[ind]
-    }
-    return domains
-})
-
-const useNativeName: string = (() => {
-    if (useCountry.value === null) {
-        return
-    }
-    const nativeName = Object.values(useCountry.value?.name.nativeName)[0].official
-    return nativeName
-})()
 
 const goBack = () => {
     let router = useRouter()
     router.back()
+}
+
+const makeLink = (code: string) => {
+    return `/country-${code}`
 }
 
 </script>
@@ -66,7 +74,10 @@ const goBack = () => {
 <template>
     <div class="">
         <div class="d-flex justify-center">
-            <div class="h-100 d-flex flex-column justify-center align-center" v-if="loading">
+            <div
+                class="h-100 d-flex flex-column justify-center align-center"
+                v-if="loading"
+            >
 
                 <p>LOading...</p>
             </div>
@@ -76,7 +87,12 @@ const goBack = () => {
         </div>
         <v-container>
 
-            <button @click="goBack" class="mb-4 "><h5 class="text-h5 font-weight-bold">Back</h5></button>
+            <button
+                @click="goBack"
+                class="mb-4 "
+            >
+                <h5 class="text-h5 font-weight-bold">Back</h5>
+            </button>
             <v-row
                 height="500"
                 class=""
@@ -105,16 +121,29 @@ const goBack = () => {
                         </v-row>
                         <v-row>
                             <v-col>
-                                <p>Native Name:{{ useNativeName }} </p>
-                                <p>Population:{{ useCountry?.population }} </p>
-                                <p>Region:{{ useCountry?.region }} </p>
-                                <p>Subregion:{{ useCountry?.subregion }} </p>
-                                <p>Captial:{{ useCapital }} </p>
+                                <p>Native Name: {{ useNative }} </p>
+                                <p>Population: {{ useCountry?.population }} </p>
+                                <p>Region: {{ useCountry?.region }} </p>
+                                <p>Subregion: {{ useCountry?.subregion }} </p>
+                                <p>Captial: {{ useCapital }} </p>
                             </v-col>
                             <v-col>
-                                <p>Top Level Domains: {{ useTopLevelDomains }}</p>
-                                <p>Curreny:</p>
-                                <p>Language:</p>
+                                <p>Curreny: {{ useCurrency }}</p>
+                                <p>Language: {{ useLanguage }}</p>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <h3 class="text-h3 mt-5 mb-2">Border Countries</h3>
+                                <v-chip
+                                    class="rounded-pill chip mx-1"
+                                    v-for="border in useBorders"
+                                >
+                                    <Nuxt-link :to="makeLink(border)">
+
+                                        {{ border }}
+                                    </Nuxt-link>
+                                </v-chip>
                             </v-col>
                         </v-row>
                     </div>
@@ -126,5 +155,24 @@ const goBack = () => {
 <style>
 #countryDescription {
     line-height: 2.1;
+}
+
+
+
+.chip a{
+    font-weight: bold;
+    color: inherit;
+    text-decoration: none;
+}
+
+.rounded-pill:hover{
+    color: white;
+}
+
+
+.chip:hover {
+    cursor: pointer;
+    background-color: black;
+    color: white;
 }
 </style>
